cdf620f0a7308bda64fd1e775002664992292674
ViktorMir <mirnenko.v.n@gmail.com>
2023-05-21 23:42:31 +0300

    points 2.3, 2.4, 2.5 are completed.

---
+++ src/Additional.java
@@ -0,0 +1,7 @@
+public class Additional {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("*");
+}
+}
---
+++ src/Emergency.java
@@ -0,0 +1,7 @@
+public class Emergency {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("my favorite numbers are 14, 25 and 4");
+    }
+}
