7d423b7882cd3ae3adfc81e86c710dd4caef280e
ViktorMir <mirnenko.v.n@gmail.com>
2023-05-25 15:57:16 +0300

    // task 2.3 2.4, 2.5 are completed.         System.out.println("my favorite color is yellow");         System.out.println("my favorite color is blue");         System.out.println("my favorite color is green"); // task 2.4         System.out.println("*"); // task 2.5         System.out.println("my favorite numbers are 14, 25 and 4");

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
         System.out.println("my favorite color is yellow");
         System.out.println("my favorite color is blue");
         System.out.println("my favorite color is green");
